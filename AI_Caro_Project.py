2f03eb331a2a9bb3d9a17f65e0c62bc40625330e
HoangDang1123 <110187412+HoangDang1123@users.noreply.github.com>
2023-11-22 16:34:37 +0700

    minimax

--- AI_Caro_Project.py
+++ AI_Caro_Project.py
@@ -3,6 +3,10 @@ from tkinter import *
 from time import *
 import tkinter as tk
 import sys
+import time
+import math
+import numpy as np
+import random
 
 class Board():
     # Kiểm tra thắng
@@ -83,9 +87,303 @@ class Board():
         for i in range(len(loa)):
             lst.append(loa[i][colNum])
         return lst
+    
+class Minimax():
+    def __init__(self):
+        self.MAX, self.MIN = math.inf, -math.inf
+        self.patterns = {
+            '11111': 30000000,
+            '22222': -30000000,
+            '011110': 20000000,
+            '022220': -20000000,
+            '011112': 50000,
+            '211110': 50000,
+            '022221': -50000,
+            '122220': -50000,
+            '01110': 30000,
+            '02220': -30000,
+            '011010': 15000,
+            '010110': 15000,
+            '022020': -15000,
+            '020220': -15000,
+            '001112': 2000,
+            '211100': 2000,
+            '002221': -2000,
+            '122200': -2000,
+            '211010': 2000,
+            '210110': 2000,
+            '010112': 2000,
+            '011012': 2000,
+            '122020': -2000,
+            '120220': -2000,
+            '020221': -2000,
+            '022021': -2000,
+            '01100': 500,
+            '00110': 500,
+            '02200': -500,
+            '00220': -500
+        }
+    
+    def getCoordsAround(self, board_size, board):
+        '''
+        get points around placed stones
+        '''
+        outTpl = np.nonzero(board)  # return tuple of all non zero points on board
+        potentialValsCoord = {}
+        for i in range(len(outTpl[0])):
+            y = outTpl[0][i]
+            x = outTpl[1][i]
+            if y > 0:
+                potentialValsCoord[(x, y-1)] = 1
+                if x > 0:
+                    potentialValsCoord[(x-1, y-1)] = 1
+                if x < (board_size-1):
+                    potentialValsCoord[(x+1, y-1)] = 1
+            if x > 0:
+                potentialValsCoord[(x-1, y)] = 1
+                if y < (board_size-1):
+                    potentialValsCoord[(x-1, y+1)] = 1
+            if y < (board_size-1):
+                potentialValsCoord[(x, y+1)] = 1
+                if x < (board_size-1):
+                    potentialValsCoord[(x+1, y+1)] = 1
+                if x > 0:
+                    potentialValsCoord[(x-1, y+1)] = 1
+            if x < (board_size-1):
+                potentialValsCoord[(x+1, y)] = 1
+                if y > 0:
+                    potentialValsCoord[(x+1, y-1)] = 1
+        finalValsX, finalValsY = [], []
+        for key in potentialValsCoord:
+            finalValsX.append(key[0])
+            finalValsY.append(key[1])
+        return finalValsX, finalValsY
 
-class GUI(Board):
+
+    def convertArrToMove(self, row, col):
+        '''
+        col goes to letter of number plus 1
+        row is number but plus 1
+        '''
+        colVal = chr(col+ord('a'))  # 97
+        rowVal = str(row+1)
+        return colVal+rowVal
+
+
+    def convertMoveToArr(self, col, row):
+        '''
+        convert move ex: a4 to be converted to col and row integers for array
+        '''
+        colVal = ord(col)-ord('a')  # double check
+        rowVal = int(row)-1
+        return colVal, rowVal
+
+
+    def convertKeyToArr(self, key):
+        '''
+        convert key in getcoordsaround func to array indexes
+        '''
+        colVal = ord(key[0])-ord('a')  # double check
+        rowVal = int(key[1:])-1
+        return colVal, rowVal
+
+
+    def getRandomMove(self, board, boardSize):
+        '''
+        For choosing random move when can't decide propogated to center
+        '''
+        ctr = 0
+        idx = boardSize//2
+        while ctr < (idx/2):
+            if board[idx+ctr][idx+ctr] == 0:
+                return idx+ctr, idx+ctr
+            elif board[idx+ctr][idx-ctr] == 0:
+                return idx+ctr, idx-ctr
+            elif board[idx+ctr][idx] == 0:
+                return idx+ctr, idx
+            elif board[idx][idx+ctr] == 0:
+                return idx, idx+ctr
+            elif board[idx][idx-ctr] == 0:
+                return idx, idx-ctr
+            elif board[idx-ctr][idx] == 0:
+                return idx-ctr, idx
+            elif board[idx-ctr][idx-ctr] == 0:
+                return idx-ctr, idx-ctr
+            elif board[idx-ctr][idx+ctr] == 0:
+                return idx-ctr, idx+ctr
+            ctr += 1
+        for i in range(boardSize):
+            for j in range(boardSize):
+                if board[i][j] == 0:
+                    return i, j
+
+    def btsConvert(self, board, player):
+        '''
+        convert board to col,row,and diag string arrays for easier interpreting 
+        '''
+        temp_board = np.array(board)
+        cList, rList, dList = [], [], []
+        board_col = len(board[0])
+        bdiag = [temp_board.diagonal(i) for i in range(board_col - 5, - board_col + 4, -1)]
+        fdiag = [temp_board[::-1, :].diagonal(i) for i in range(board_col - 5, - board_col + 4, -1)]
+        for dgd in bdiag:
+            bdiagVals = ""
+            for point in dgd:
+                if point == 0:
+                    bdiagVals += "0"
+                elif point == player:
+                    bdiagVals += "1"
+                else:
+                    bdiagVals += "2"
+            dList.append(bdiagVals)
+        for dgu in fdiag:
+            fdiagVals = ""
+            for point in dgu:
+                if point == 0:
+                    fdiagVals += "0"
+                elif point == player:
+                    fdiagVals += "1"
+                else:
+                    fdiagVals += "2"
+            dList.append(fdiagVals)
+        boardT = temp_board.copy().transpose()
+        for col in boardT:
+            colVals = ""
+            for point in col:
+                if point == 0:
+                    colVals += "0"
+                elif point == player:
+                    colVals += "1"
+                else:
+                    colVals += "2"
+            cList.append(colVals)
+        for row in board:
+            rowVals = ""
+            for point in row:
+                if point == 0:
+                    rowVals += "0"
+                elif point == player:
+                    rowVals += "1"
+                else:
+                    rowVals += "2"
+            rList.append(rowVals)
+        return dList+cList+rList
+
+
+    def points(self, board, player):  # evaluates
+        '''
+        assigns points for moves
+        '''
+        val = 0
+        player1StrArr = self.btsConvert(board, player)
+        for i in range(len(player1StrArr)):
+            len1 = len(player1StrArr[i])
+            for j in range(len1):
+                n = j+5
+                if(n <= len1):
+                    st = player1StrArr[i][j:n]
+                    if st in self.patterns:
+                        val += self.patterns[st]
+            for j in range(len1):
+                n = j+6
+                if(n <= len1):
+                    st = player1StrArr[i][j:n]
+                    if st in self.patterns:
+                        val += self.patterns[st]
+        return val
+
+    def otherPlayerStone(self, player):
+        '''
+        Stones are 1 or 2 based on player. Just gets other player's stone
+        '''
+        return 2 if player==1 else 1
+
+    def minimax(self, board, isMaximizer, depth, alpha, beta, player):  # alpha, beta
+        '''
+        Minimax with Alpha-Beta pruning (also computer is 1st Max in this implementation)
+        alpha is best already explored option along path to root for maximizer(AI)
+        beta is best already explored option along path to root for minimizer(AI Opponent)
+        '''
+        
+        point = self.points(board, player)
+        if depth == 2 or point>=20000000 or point<=-20000000:
+            return point
+        size = len(board)
+        if isMaximizer:  # THE MAXIMIZER
+            best = self.MIN
+            potentialValsX, potentialValsY = self.getCoordsAround(size, board)
+            for i in range(len(potentialValsX)):
+                if board[potentialValsY[i]][potentialValsX[i]] == 0:
+                    board[potentialValsY[i]][potentialValsX[i]] = player
+                    score = self.minimax(board, False, depth+1, alpha, beta, player)
+                    best = max(best, score)
+                    alpha = max(alpha, best)  # best AI Opponent move
+                    board[potentialValsY[i]][potentialValsX[i]] = 0  # undoing
+                    if beta <= alpha:
+                        break
+            return best
+        else:  # THE MINIMIZER
+            best = self.MAX
+            potentialValsX, potentialValsY = self.getCoordsAround(size, board)
+            for i in range(len(potentialValsX)):
+                if board[potentialValsY[i]][potentialValsX[i]] == 0:
+                    otherplayer = self.otherPlayerStone(player)
+                    board[potentialValsY[i]][potentialValsX[i]] = otherplayer
+                    score = self.minimax(board, True, depth+1, alpha, beta, player)
+                    best = min(best, score)
+                    beta = min(beta, best)  # best AI Opponent move
+                    board[potentialValsY[i]][potentialValsX[i]] = 0  # undoing
+                    if beta <= alpha:
+                        break
+                    if movePoints > mostPoints:
+                        bestMoveRow = potentialValsY[i]
+                        bestMoveCol = potentialValsX[i]
+                        mostPoints = movePoints
+                        if movePoints >= 20000000:
+                            break
+            if bestMoveRow == -1 or bestMoveCol == -1:  # ' when still -1
+                bestMoveRow, bestMoveCol = self.getRandomMove(board, boardSize)
+            board[bestMoveRow][bestMoveCol] = mark
+            return bestMoveRow, bestMoveCol
+            return best
+
+    def computer(self, board_size, board, isComputerFirst):
+        '''
+        Chooses best move for computer
+        Max that gives index and calls min in minimax 
+        '''
+        mostPoints = float('-inf')
+        alpha,beta = self.MIN, self.MAX
+        if isComputerFirst:
+            mark = 1
+        else:
+            mark = 2
+        bestMoveRow = bestMoveCol = -1
+        boardSize = len(board)
+        potentialValsX, potentialValsY = self.getCoordsAround(board_size, board)
+        for i in range(len(potentialValsX)):
+            if board[potentialValsY[i]][potentialValsX[i]] == 0:
+                board[potentialValsY[i]][potentialValsX[i]] = mark
+                movePoints = max(mostPoints, self.minimax(
+                    board, False, 1, alpha, beta, mark))
+                alpha = max(alpha, movePoints)
+                board[potentialValsY[i]][potentialValsX[i]] = 0
+                if beta <= alpha:
+                    break
+                if movePoints > mostPoints:
+                    bestMoveRow = potentialValsY[i]
+                    bestMoveCol = potentialValsX[i]
+                    mostPoints = movePoints
+                    if movePoints >= 20000000:
+                        break
+        if bestMoveRow == -1 or bestMoveCol == -1:  # ' when still -1
+            bestMoveRow, bestMoveCol = self.getRandomMove(board, boardSize)
+        board[bestMoveRow][bestMoveCol] = mark
+        return bestMoveRow, bestMoveCol
+
+class GUI(Board, Minimax):
     b = Board()
+    m = Minimax()
     def __init__(self):
         self.window = tk.Tk()
         self.window.title("ĐỒ ÁN HỌC PHẦN - LẬP TRÌNH GAME CỜ CARO")
@@ -515,15 +813,15 @@ class GUI(Board):
         else:
             self.piece_num = self.black_piece
             
+            
         while self.winner == None:
             x = 0
             y = 0
             if(self.AI):
                 if(self.AI_turn):
                     if(self.AI_algorithm):
-                        # Thuật toán 1 => x, y
-                        x = 0
-                        y = 0
+                        x, y = self.m.computer(self.board_size, self.board, self.AI_turn)
+                        print(x, y)
                     else:
                         # Thuật toán 2
                         x = 0
